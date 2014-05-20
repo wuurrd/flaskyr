bddfbad3cfe23ae6e8d90712fc45c484de17efd0
David Buchmann <davbuchm@cisco.com>
2014-05-20 16:18:00 +0200

    Slightly more working prototype

--- flaskyr/app.py
+++ flaskyr/app.py
@@ -20,5 +20,15 @@ def get_yr(location):
     data = xmltodict.parse(data)
     return jsonify(data)
 
+@app.route('/weather/longterm/json/<path:location>')
+def get_longterm_yr(location):
+    #url = 'http://www.yr.no/sted/Norge/Akershus/B%C3%A6rum/Halden_brygge/varsel_time_for_time.xml'
+    url = 'http://www.yr.no/sted/%s/forecast.xml' % (location)
+    response = requests.get(url)
+    content = response.content
+    data = content
+    data = xmltodict.parse(data)
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run()
